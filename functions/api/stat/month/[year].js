4dc40a3a9fef3a662d20e6efe5aa3c5084751271
lll69 <60803753+lll69@users.noreply.github.com>
2026-05-31 15:28:05 +0800

    add year and month api

---
+++ functions/api/stat/month/[year].js
@@ -0,0 +1,98 @@
+const JSON_TYPE = {
+    "content-type": "application/json",
+};
+const JSON_WITH_CACHE = {
+    "content-type": "application/json",
+    "Cache-Control": "max-age=21600",
+};
+
+function convertTime(item) {
+    return typeof item === "number" ? item : item[0];
+}
+
+function getYear(time) {
+    return new Date(time * 1000).getUTCFullYear();
+}
+
+function getMonthData(timeData, requestedYear) {
+    const minYear = getYear(Math.abs(convertTime(timeData[0])));
+    const maxYear = getYear(Math.abs(convertTime(timeData[timeData.length - 1])));
+    if (requestedYear < minYear || requestedYear > maxYear) {
+        return null;
+    }
+    const result = Array(12);
+    for (let i = 0; i < 11; i++) {
+        result[i] = 0;
+    }
+    for (const item of timeData) {
+        if (typeof item === "number") {
+            const date = new Date(Math.abs(item) * 1000);
+            const year = date.getUTCFullYear();
+            if (year !== requestedYear) continue;
+            const month = date.getUTCMonth();
+            result[month] += Math.sign(item);
+        } else {
+            const time = item[0];
+            const date = new Date(Math.abs(time) * 1000);
+            const year = date.getUTCFullYear();
+            if (year !== requestedYear) continue;
+            const month = date.getUTCMonth();
+            result[month] += item[1];
+        }
+    }
+    return result;
+}
+
+export async function onRequestGet({ request, env, params }) {
+    const yearStr = String(params.year).trim();
+    const year = parseInt(yearStr);
+    if (isNaN(year)) {
+        return new Response(JSON.stringify({
+            code: 400,
+            status: "INVALID_INPUT",
+        }), { status: 400, headers: JSON_WITH_CACHE });
+    }
+    const url = "https://api.github.com/repos/lll69/js-org-stats/contents/times.json?ref=stat";
+    try {
+        const response = await fetch(url, {
+            method: "GET",
+            headers: {
+                "Accept": "application/vnd.github.raw+json",
+                "Authorization": "Bearer " + env.GITHUB_TOKEN,
+                "User-Agent": "LookupJsOrg",
+            },
+            cf: {
+                cacheTtl: 21600,
+                cacheEverything: true,
+            }
+        });
+        const text = await response.text();
+        if (!response.ok) {
+            return new Response(JSON.stringify({
+                code: 500,
+                status: "UPSTREAM_ERROR",
+                upstreamCode: response.status,
+                data: text
+            }), { status: 500, headers: JSON_TYPE });
+        }
+        const jsonData = JSON.parse(text);
+        const data = getMonthData(jsonData.data, year);
+        if (data === null) {
+            return new Response(JSON.stringify({
+                code: 404,
+                status: "YEAR_NOT_FOUND",
+            }), { status: 404, headers: JSON_WITH_CACHE });
+        }
+        const result = {
+            code: 200,
+            status: "SUCCESS",
+            updateTime: jsonData["^updateTime"],
+            data: data,
+        }
+        return new Response(JSON.stringify(result), { status: 200, headers: JSON_WITH_CACHE });
+    } catch (e) {
+        return new Response(JSON.stringify({
+            code: 500,
+        }), { status: 500, headers: JSON_TYPE });
+    }
+}
---
+++ functions/api/stat/years.js
@@ -0,0 +1,75 @@
+const JSON_TYPE = {
+    "content-type": "application/json",
+};
+const JSON_WITH_CACHE = {
+    "content-type": "application/json",
+    "Cache-Control": "max-age=21600",
+};
+
+function convertTime(item) {
+    return typeof item === "number" ? item : item[0];
+}
+
+function getYear(time) {
+    return new Date(time * 1000).getUTCFullYear();
+}
+
+function getYearData(timeData) {
+    const minYear = getYear(Math.abs(convertTime(timeData[0])));
+    const maxYear = getYear(Math.abs(convertTime(timeData[timeData.length - 1])));
+    const result = {};
+    for (let i = minYear; i <= maxYear; i++) {
+        result[i] = { "+": 0, "-": 0 };
+    }
+    for (const item of timeData) {
+        if (typeof item === "number") {
+            const year = getYear(Math.abs(item));
+            result[year][item < 0 ? "-" : "+"]++;
+        } else {
+            const time = item[0];
+            const year = getYear(Math.abs(time));
+            result[year][time < 0 ? "-" : "+"] += item[1];
+        }
+    }
+    return result;
+}
+
+export async function onRequestGet({ request, env, params }) {
+    const url = "https://api.github.com/repos/lll69/js-org-stats/contents/times.json?ref=stat";
+    try {
+        const response = await fetch(url, {
+            method: "GET",
+            headers: {
+                "Accept": "application/vnd.github.raw+json",
+                "Authorization": "Bearer " + env.GITHUB_TOKEN,
+                "User-Agent": "LookupJsOrg",
+            },
+            cf: {
+                cacheTtl: 21600,
+                cacheEverything: true,
+            }
+        });
+        const text = await response.text();
+        if (!response.ok) {
+            return new Response(JSON.stringify({
+                code: 500,
+                status: "UPSTREAM_ERROR",
+                upstreamCode: response.status,
+                data: text
+            }), { status: 500, headers: JSON_TYPE });
+        }
+        const jsonData = JSON.parse(text);
+        const data = getYearData(jsonData.data);
+        const result = {
+            code: 200,
+            status: "SUCCESS",
+            updateTime: jsonData["^updateTime"],
+            data: data,
+        }
+        return new Response(JSON.stringify(result), { status: 200, headers: JSON_WITH_CACHE });
+    } catch (e) {
+        return new Response(JSON.stringify({
+            code: 500,
+        }), { status: 500, headers: JSON_TYPE });
+    }
+}
